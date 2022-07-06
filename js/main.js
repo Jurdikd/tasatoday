976f1e60f5338c36643d3d9238a017a54fa0c163
Jurdikd <jesuscovo@gmail.com>
2022-07-06 09:15:26 -0400

    configuracion general

--- js/main.js
+++ js/main.js
@@ -4,40 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // inputs
-
 const amount = document.getElementById("amount"); // cantidad
 const result = document.getElementById("result"); // resultado
 // labels
 const labelAmount = document.getElementsByClassName("labelAmount"); // etiqueta cantidad
 const labelResult = document.getElementsByClassName("labelResult"); // etiqueta resultado
-// botones
-
+// buttons
 const changeDivisa = document.getElementById("changeDivisa");
 const tittleDivisa = document.getElementsByClassName("tittleDivisa");
 
-const fetchDivisa = async (verify) => {
-	try {
-		console.log();
-
-		const res = await fetch("backend/api_divisa.php", {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(verify),
-		});
-		const data = await res.json();
-		return data;
-		//console.log(data);*/
-	} catch (error) {
-		console.log(error);
-	}
-};
-//
-
-// Converir automatico
-amount.addEventListener("keyup", (e) => {
+// Automatic convert
+amount.addEventListener("keyup", () => {
 	//console.log(e.target.value);
 	const verify = {
 		verify: 2,
@@ -53,6 +30,7 @@ amount.addEventListener("keyup", (e) => {
 		convert_BS_to_EUR(verify);
 	}
 });
+// Convert divisa with a click on change
 changeDivisa.addEventListener("click", (e) => {
 	// Cambio de moneda
 	//console.log(e.target.innerText);
@@ -70,18 +48,51 @@ changeDivisa.addEventListener("click", (e) => {
 		convert_BS_to_USD(verify);
 	}
 });
-
-// Convertir Bs a USD
-async function convert_BS_to_USD(verify) {
+/*========================== Functions =================================================*/
+// Fetch for to consult
+const fetchDivisa = async (verify) => {
+	try {
+		const res = await fetch("backend/api_divisa.php", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(verify),
+		});
+		const data = await res.json();
+		return data;
+		//console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+/*========================== Calculates =================================================*/
+// Convert Bs to USD
+const convert_BS_to_USD = async (verify) => {
 	const datos = await fetchDivisa(verify);
 	//console.log(datos);
 	let convertion = amount.value / datos.usd.tasa;
 	result.value = convertion.toFixed(2);
-}
-// Convertir Bs a EUR
-async function convert_BS_to_EUR(verify) {
+};
+// Convert Bs to EUR
+const convert_BS_to_EUR = async (verify) => {
 	const datos = await fetchDivisa(verify);
 	//console.log(datos);
 	let convertion = amount.value / datos.eur.tasa;
 	result.value = convertion.toFixed(2);
-}
+};
+// Convert USD to BS
+const convert_USD_to_BS = async (verify) => {
+	const datos = await fetchDivisa(verify);
+	//console.log(datos);
+	let convertion = amount.value * datos.usd.tasa;
+	result.value = convertion.toFixed(2);
+};
+// Convert EUR to BS
+const convert_EUR_to_BS = async (verify) => {
+	const datos = await fetchDivisa(verify);
+	//console.log(datos);
+	let convertion = amount.value * datos.eur.tasa;
+	result.value = convertion.toFixed(2);
+};
