479a30ded398bd14eed8f722850405154015929b
Jurdikd <jesuscovo@gmail.com>
2022-07-08 15:03:25 -0400

    modificacion de libreria

--- js/calcualteTodayTerror.js
+++ js/calcualteTodayTerror.js
@@ -1,88 +1,76 @@
 export const calcualteTodayTerror = {
 	// Fetch for to consult
-	fetchDivisa: async (verify, option) => {
+	fetchDivisa: async (rates) => {
 		try {
-			let res = "";
-			if (option === true) {
-				res = await fetch("backend/api_divisa.php", {
-					method: "POST",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify(verify),
-				});
-				const data = await res.json();
-				//console.log(data);
-				return data;
-			} else if (option === false) {
-				res = await fetch("https://s3.amazonaws.com/dolartoday/data.json");
-				const datos = await res.json();
-				const data = {
-					date: {
-						dia: datos._timestamp.dia,
-						fecha: datos._timestamp.fecha,
-					},
-					eur: {
-						tasa: datos.EUR.sicad2,
-						tasa2: datos.EUR.sicad1,
-						promedio: datos.EUR.promedio_real,
-					},
-					usd: {
-						tasa: datos.USD.sicad2,
-						tasa2: datos.USD.sicad1,
-						promedio: datos.USD.promedio_real,
-					},
-				};
-				//console.log(data);
-				return data;
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	fetchDivisaLocalbitcoin: async () => {
-		try {
-			const res = await fetch("backend/api_local.php");
-
-			const datos = await res.json();
-			const data = {
-				tasa: datos.VED.rates.last,
-				tasa24h: datos.VED.avg_24h,
-				volume_btc: datos.volume_btc,
-			};
-			//console.log(data);
+			const res = await fetch("app/ajax/divisa.ajax.php", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(rates),
+			});
+			const data = await res.json();
+			console.log(data);
 			return data;
 		} catch (error) {
 			console.log(error);
 		}
 	},
-	//Convert Bs to USD
-	bcv_BS_to_USD: async (verify, option) => {
-		const datos = await calcualteTodayTerror.fetchDivisa(verify, option);
-		//console.log(datos);
-		let convertion = amount.value / datos.usd.tasa;
+	//Convert Bcv to USD
+	bcv_to_usd: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value / rate.bcv.tasa;
 		result.value = convertion.toFixed(2);
 	},
-	// Convert Bs to EUR
-	bcv_BS_to_EUR: async (verify, option) => {
-		const datos = await calcualteTodayTerror.fetchDivisa(verify, option);
-		//console.log(datos);
-		let convertion = amount.value / datos.eur.tasa;
+	// Convert usd to bcv
+	usd_to_bcv: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value / rate.bcv.tasa;
 		result.value = convertion.toFixed(2);
 	},
-	// Convert USD to BS
-	USD_to_BS: async (verify, option) => {
-		const datos = await calcualteTodayTerror.fetchDivisa(verify, option);
-		//console.log(datos);
-		let convertion = amount.value * datos.usd.tasa;
+	// Convert enparalelo to usd
+	enparalelo_to_usd: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value * rate.enparalelo.tasa;
 		result.value = convertion.toFixed(2);
 	},
-	// Convert EUR to BS
-	EUR_to_BS: async (verify, option) => {
-		const datos = await calcualteTodayTerror.fetchDivisa(verify, option);
-		//console.log(datos);
-		let convertion = amount.value * datos.eur.tasa;
+	// Convert usd to enparalelo
+	usd_to_enparalelo: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value / rate.enparalelo.tasa;
+		result.value = convertion.toFixed(2);
+	},
+	// Convert airtm to usd
+	airtm_to_usd: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value * rate.airtm.tasa;
+		result.value = convertion.toFixed(2);
+	},
+	// Convert usd to airtm
+	usd_to_airtm: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value / rate.airtm.tasa;
+		result.value = convertion.toFixed(2);
+	},
+	// Convert zelle to usd
+	zelle_to_usd: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value * rate.zelle.tasa;
+		result.value = convertion.toFixed(2);
+	},
+	// Convert usd to zelle
+	usd_to_zelle: async (rates) => {
+		const rate = await calcualteTodayTerror.fetchDivisa(rates);
+		//console.log(rate);
+		let convertion = amount.value / rate.zelle.tasa;
 		result.value = convertion.toFixed(2);
 	},
 };
