c5488d6ec7e2a81b1163200cad7813c42c19c320
Jurdikd <jesuscovo@gmail.com>
2022-07-08 15:00:45 -0400

    borrado de  archivos

--- backend/api_local.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "../libs/CurlTerror.inc.php";
-$result = CurlTerror::get_page('https://localbitcoins.com/bitcoinaverage/ticker-all-currencies');
-header('Content-Type: application/json'); # declarar json
-echo $result;
