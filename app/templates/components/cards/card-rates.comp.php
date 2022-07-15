4a46d33c79cfbdca2cf9f2f8ec8b81c928906829
Jurdikd <jesuscovo@gmail.com>
2022-07-15 13:28:19 -0400

    carga de template

---
+++ app/templates/components/cards/card-rates.comp.php
@@ -0,0 +1,14 @@
+<!-- templates cards -->
+<template id="card-rate">
+   <div class="col-md-3">
+        <div class="card m-2">
+            <div class="card-header text-center">
+                <h5 class="card-tittle">Name rate</h5>
+            </div>
+            <div class="card-body text-center">
+                <span class="tasatoday-rate">0.00</span>
+                <button class="btn btn-sm btn-success btn-setRate" type="button" data-setRate="Name rate">Elegir</button>
+            </div>
+        </div>
+    </div>
+</template>
