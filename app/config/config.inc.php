c3c24d984492297001660656308b660cf062f932
Jurdikd <jesuscovo@gmail.com>
2022-07-18 14:44:36 -0400

    cambio de solucion de if

--- app/config/config.inc.php
+++ app/config/config.inc.php
@@ -22,12 +22,12 @@ http://localhost/instarapid/
 define("HTTPS", $_SERVER["REQUEST_SCHEME"] . "://");
 define("DOMINIO", $_SERVER['SERVER_NAME']);
 define("PUERTO", $_SERVER['SERVER_PORT']);
-if (PUERTO !== "80" || PUERTO !== "443") {
+if (PUERTO === "80" || PUERTO === "443") {
     echo "entro en el if <br>";
-    define("SERVIDOR", HTTPS . DOMINIO . ":" . PUERTO . "/");
+    define("SERVIDOR", HTTPS . DOMINIO . "/");
 } else {
     echo "entro en el else <br>";
-    define("SERVIDOR", HTTPS . DOMINIO . "/");
+    define("SERVIDOR", HTTPS . DOMINIO . ":" . PUERTO . "/");
 }
 if (PUERTO === 443) {
     # code...
