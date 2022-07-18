6488c3e0fe213303de4c85a3ebfe02a65ffff2ad
Jurdikd <jesuscovo@gmail.com>
2022-07-18 15:11:02 -0400

    configuracion de error 404

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ $partes_ruta = explode('/', $ruta);
 $partes_ruta = array_filter($partes_ruta);
 $partes_ruta = array_slice($partes_ruta, 0);
 
-if ($partes_ruta[0] == DOMINIO) { #DOMINIO ES IGUAL A LA RUTA PREDFINIDA
-    if (count($partes_ruta) == 1) {
-        $ruta_elegida = 'view/inicio.php';
-    } else if (count($partes_ruta) == 2) {
-        // Buscamos los casos comunes
-        if ($partes_ruta[1] == 'javascript-no-activo') {
-            # code...
-            $ruta_elegida = 'view/sin-js.php';
-        } else {
-            //Error 404
-            $ruta_elegida = 'view/404.php';
-        }
+//Error 404
+$ruta_elegida = 'view/404.php';
+// Vistas seleccionadas
+if (count($partes_ruta) == 1) {
+    $ruta_elegida = 'view/inicio.php';
+} else if (count($partes_ruta) == 2) {
+    // Buscamos los casos comunes
+    if ($partes_ruta[1] == 'javascript-no-activo') {
+        # code...
+        $ruta_elegida = 'view/sin-js.php';
     }
 }
+
 include_once $ruta_elegida;
