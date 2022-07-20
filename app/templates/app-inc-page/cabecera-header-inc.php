9ff6ace6db11e71fc105dc058417eae1b3a9e91c
Jurdikd <jesuscovo@gmail.com>
2022-07-20 12:38:26 -0400

    no cache

--- app/templates/app-inc-page/cabecera-header-inc.php
+++ app/templates/app-inc-page/cabecera-header-inc.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">
+    <meta http-equiv="pragma" content="no-cache">
     <noscript>
-        <meta HTTP-EQUIV="REFRESH" content="0; url=javascript-no-activo">
+        <meta HTTP-EQUIV=" REFRESH" content="0; url=javascript-no-activo">
     </noscript>
     <title>TasaToday</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
