d8f3bdd9bc8b6fe65f376615d1ba6ecde0e68980
Jurdikd <jesuscovo@gmail.com>
2022-07-20 11:55:07 -0400

    nuevos estilos para error 404

--- view/404.php
+++ view/404.php
@@ -1,4 +1,29 @@
 <?php
 header($_SERVER['SERVER_PROTOCOL'] . "404 Not Found", true, 404);
 header("HTTP/1.0 404 Not Found");
-echo "error 404";
+
+include_once "app/templates/app-inc-page/cabecera-header-inc.php"; ?>
+
+<div class="container">
+    <div class="row p-5 m-4">
+        <div class="col-6 mx-auto">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-tiitle">Error 404</h5>
+                </div>
+                <div class="card-body">
+                    <p>Lo que buscas no ha sido encotrado...</p>
+                </div>
+                <div class="footer text-center">
+                    <a class="btn btn-link btn-sm p-4" href="<?php echo SERVIDOR; ?>">Volver al inicio</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+include_once "app/templates/app-inc-page/cuerpo-body-close.inc.php";
+include_once "app/templates/app-inc-page/pie-footer.inc.php";
+?>
