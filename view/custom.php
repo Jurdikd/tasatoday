0a9356e28496b7e5304d90c3481b5b6e59f488c0
Jurdikd <jesuscovo@gmail.com>
2022-07-20 13:52:33 -0400

    mejora de posicionamiento de contenido

--- view/custom.php
+++ view/custom.php
@@ -9,29 +9,29 @@ $name = UrlGetTerror::Getquery("name");
 $titulo = "Custom";
 include_once "app/templates/app-inc-page/cabecera-header-inc.php";
 ?>
-
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-10  mx-auto m-4">
-            <div class="h-100 p-5 text-white bg-dark rounded-3">
-                <h1>En construcción... TasaToday</h1>
-                <p class="lead">Nuestra misión no es hacer una simple calculadora o ver las tasas al momento...</p>
-                <br>
-                <p class="lead">Para nosotros lo importante es que puedas analizar cuanto ganas o pierdes por tasas
-                    ¡incluso hasta personalizadas por tí!
-                    para ello estamos construyendo este sitio y puedas compartir tu tasa por whatsapp o agregarlo a tu
-                    negocio y saber cuanto cotizas al instante solo espera y te sorprenderemos
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="row">
+<div class="container">
+    <div class="row m-2">
 
         <div class="col-md-4 card-calculator mx-auto">
 
         </div>
 
     </div>
+    <div class="row m-2">
+        <div class="col-md-10  mx-auto m-4">
+            <div class="h-100 p-5 text-white bg-dark rounded-3 jumbotron">
+                <h1>En construcción... TasaToday</h1>
+                <p class="lead">Nuestra misión no es hacer una simple calculadora o ver las tasas al momento...</p>
+                <br>
+                <p class="lead">Para nosotros lo importante es que puedas analizar cuanto ganas o pierdes por tasas
+                    ¡incluso hasta personalizadas por tí!
+                    para ello estamos construyendo este sitio y puedas compartir tu tasa por whatsapp o agregarlo a tu
+                    negocio y saber cuanto cotizas al instante solo espera y te sorprenderemos!!
+                </p>
+            </div>
+        </div>
+    </div>
+
 </div>
 <?php
 include_once "app/templates/components/cards/card-rates.comp.php";
