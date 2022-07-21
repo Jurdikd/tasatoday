76b30f7cb8ba9fed152c0e070e80f1b3074b6025
Jurdikd <jesuscovo@gmail.com>
2022-07-21 10:37:57 -0400

    actualizacion para crear tasa personalizada

--- app/templates/app-inc-page/cuerpo-body-close.inc.php
+++ app/templates/app-inc-page/cuerpo-body-close.inc.php
@@ -1,11 +1,25 @@
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
     id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Crear tu propia tasa</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-        <p>Try scrolling the rest of the page to see this option in action.</p>
+        <p>¡Puedes crear tasas con el nombre de tú negocio o el que quieras y el valor al cambio que prefieras!</p>
+        <div class="row g-3">
+            <div class="col-auto">
+                <label for="nameCreate" class="visually-hidden">Nombre de negocio</label>
+                <input type="text" class="form-control-plaintext" id="nameCreate" name="nameCreate" value="MiNegocio">
+            </div>
+            <div class="col-auto">
+                <label for="rateCreate" class="visually-hidden">Precio de tasa en VES</label>
+                <input type="number" class="form-control" id="rateCreate" name="rateCreate" placeholder="5.70" value="">
+            </div>
+            <div class="col-auto">
+                <button type="BUTTON" class="btn btn-primary mb-3">Crear tasa</button>
+            </div>
+        </div>
+        <h4><?php echo SERVIDOR;  ?>custom?rate=6&currency=usd&to=ves&name=bendiburger</h4>
     </div>
 </div>
 <?php include_once "app/templates/components/buttons/btn_donate.com.php" ?>
--- view/js/app.js
+++ view/js/app.js
@@ -8,3 +8,23 @@ document.addEventListener("DOMContentLoaded", async () => {
 	// Loader
 	loadingTerror.load(document.getElementById("preloader"), 5000);
 });
+
+const rateCreateContent = document.querySelector(".offcanvas-body");
+
+rateCreateContent.addEventListener("change", async (e) => {
+	e.stopPropagation();
+	await createNameRate(e);
+	await createValueRate(e);
+});
+const createNameRate = async (e) => {
+	if (e.target && e.target.name === "nameCreate") {
+		console.log(e, e.target);
+		const nameCreate = e.target;
+	}
+};
+const createValueRate = async (e) => {
+	if (e.target && e.target.name === "rateCreate") {
+		console.log(e, e.target);
+		const rateCreate = e.target;
+	}
+};
