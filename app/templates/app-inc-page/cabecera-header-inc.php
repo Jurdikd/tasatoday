b34bab5cc8272c4bd2b236a7ab90feb25141a989
Jurdikd <jesuscovo@gmail.com>
2022-07-30 12:39:42 -0400

    mejora de metas

--- app/templates/app-inc-page/cabecera-header-inc.php
+++ app/templates/app-inc-page/cabecera-header-inc.php
@@ -3,7 +3,10 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="apple-mobile-web-app-able-content" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="cache-control" content="no-cache">
     <meta http-equiv="expires" content="0">
     <meta http-equiv="pragma" content="no-cache">
