2568e496a8f0ecec35f22cc61dd584a48625aa95
Jurdikd <jesuscovo@gmail.com>
2022-07-30 17:49:40 -0400

    anexo de archivos de bootstrap

--- app/templates/app-inc-page/cabecera-header-inc.php
+++ app/templates/app-inc-page/cabecera-header-inc.php
@@ -32,8 +32,7 @@
     <meta name="msapplication-config" content="<?php echo RUTA_FAVICON; ?>browserconfig.xml" />
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="<?php echo RUTA_CSS ?>bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo RUTA_CSS ?>style.css">
     <?php if (PUERTO === "80" || PUERTO === "443") {
         # mostramos analiticas
--- app/templates/app-inc-page/cuerpo-body-close.inc.php
+++ app/templates/app-inc-page/cuerpo-body-close.inc.php
@@ -32,8 +32,7 @@
 <?php include_once "app/templates/components/buttons/btn_donate.com.php" ?>
 
 <!-- close body -->
-<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-</script>
+<!-- javascript-->
+<script src="<?php echo RUTA_CSS ?>bootstrap.min.js"></script>
 <script src="<?php echo RUTA_JS ?>app.js" type="module">
 </script>
